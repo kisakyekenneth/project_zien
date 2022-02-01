5beaa7e43787e6330e8835cc6c9570b817338ae5
kiken-eng <kanyecope@gmail.com>
2022-02-01 22:38:29 +0300

    added simple function and returned response

--- playground/views.py
+++ playground/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+
+def say_hello(request):
+    # Here we can pull data from db, send email
+    return HttpResponse('Hello world')
